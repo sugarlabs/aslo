323687991285c24b1f487e49fd169bdfc118c669
fligtar@gmail.com <fligtar@gmail.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-06-20 00:18:01 +0000

    adding ru locale to utils
    git-svn-id: http://svn.mozilla.org/addons/trunk@28109 4eb1ac78-321c-0410-a911-ec516a8615a5

--- bandwagon/utils/merge-to-locales.sh
+++ bandwagon/utils/merge-to-locales.sh
@@ -2,7 +2,7 @@
 # usage: ./merge-to-locales.sh {locale file to merge into} {new strings to add}
 # example: ./merge-to-locales.sh extensionsOverlay.dtd new.txt
 
-svn_locales=(ar ca cs da de fa fr he ja pt-BR pt-PT ro sq sv-SE uk)
+svn_locales=(ar ca cs da de fa fr he ja pt-BR pt-PT ro ru sq sv-SE uk)
 bz_locales=(el-GR es-ES fy-NL id it nl pl sk vi zh-CN zh-TW)
 
 for locale in ${svn_locales[@]}
