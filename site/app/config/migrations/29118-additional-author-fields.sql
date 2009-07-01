5dd36e339ad8e666a66c6bdd925739cde97a75a6
fwenzel@mozilla.com <fwenzel@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-01 20:11:24 +0000

    do not convert cake sessions to blob (bug 501740 comment 2)
    git-svn-id: http://svn.mozilla.org/addons/trunk@29223 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/config/migrations/29118-additional-author-fields.sql
+++ site/app/config/migrations/29118-additional-author-fields.sql
@@ -3,6 +3,3 @@ alter table users
   add column `occupation` varchar(255) default '' not null, 
   add column `picture_data` mediumblob default null,
   add column `picture_type` varchar(25) default '' not null;
-
--- Argh
-alter table cake_sessions change column data data blob default null;
