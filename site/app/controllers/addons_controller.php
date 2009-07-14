60acb2b5649cb90f7f8202f2e2f583ac0358eb23
wclouser@mozilla.com <wclouser@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-14 16:39:29 +0000

    escape stuff going to the view; bug 503968
    git-svn-id: http://svn.mozilla.org/addons/trunk@29730 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/controllers/addons_controller.php
+++ site/app/controllers/addons_controller.php
@@ -175,14 +175,17 @@ class AddonsController extends AppController
         $this->set('authors', $authors);
         $this->set('num_authors', count($authors));
         $this->set('multiple', count($authors) > 1);
-        $this->set('addon', $addon);
-        $this->set('other_addons', $other_addons);
+        $this->publish('addon', $addon);
+        $this->publish('other_addons', $other_addons);
         $this->set('post_install', $extra === 'post_install');
         $this->set('roadblock', $extra === 'roadblock');
         $this->publish('breadcrumbs', array(
             sprintf(___('addons_home_pagetitle'), APP_PRETTYNAME) => '/',
             $addon['Translation']['name']['string'] => '/addon/'.$addon['Addon']['id'],
         ));
+
+        $this->pageTitle = sprintf(n___('addon_developer_title', 'addon_developer_title',
+                                count($authors)), $addon['Translation']['name']['string']);
     }
 
     /**
