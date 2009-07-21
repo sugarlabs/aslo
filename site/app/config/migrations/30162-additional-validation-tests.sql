4b419d52b3708bf0b25b488b9f7d8bbd1555934b
rwalsh@mozilla.com <rwalsh@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-21 18:50:56 +0000

    Additional validation tests SQL migration
    git-svn-id: http://svn.mozilla.org/addons/trunk@30164 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ site/app/config/migrations/30162-additional-validation-tests.sql
@@ -0,0 +1,3 @@
+ INSERT INTO `test_groups` (`id`, `category`, `tier`, `critical`, `types`) VALUES (3,'l10n',2,0,3);
+ 
+ INSERT INTO `test_cases` (`id`, `test_group_id`, `help_link`, `function`) VALUES (14,1,NULL,'all_general_checkJSPollution'),(24,2,NULL,'all_security_libraryChecksum');
