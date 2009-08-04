9b3dace84278caf1da506118656411a2773c97d6
morgamic@mozilla.com <morgamic@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-04 00:54:31 +0000

    Checking in metadata fixes for Flash.  Bug 416396.
    git-svn-id: http://svn.mozilla.org/addons/trunk@48486 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/webroot/services/pfs.php
+++ site/app/webroot/services/pfs.php
@@ -120,9 +120,12 @@ if (!empty($mimetype) &&
             $guid = '{4cfaef8a-a6c9-41a0-8e6f-967eb8f49143}';
             $XPILocation = null;
             $licenseURL = null;
+            $version = '10.0.32';
             $InstallerHash = 'sha256:f158f44911146b61f0dea0851fb35cdc812f0786297ba11745fba32a4f8b06d2';
             $InstallerLocation = 'http://fpdownload2.macromedia.com/pub/flashplayer/current/FP_PL_PFS_INSTALLER.exe';
             $iconUrl = 'http://fpdownload2.macromedia.com/pub/flashplayer/current/fp_win_installer.ico';
+            $needsRestart = 'false';
+            $InstallerShowsUI = 'false';
         } else if (preg_match('/^Win/', $clientOS)) {
             $guid = '{4cfaef8a-a6c9-41a0-8e6f-967eb8f49143}';
             $XPILocation = 'http://fpdownload.macromedia.com/get/flashplayer/xpi/current/flashplayer-win.xpi';
