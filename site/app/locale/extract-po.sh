abd111046de802234012f920f1b7ff8ac3cef63b
wclouser@mozilla.com <wclouser@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-14 04:44:33 +0000

    Adjust extraction script to work with real .po files.  This will now automatically merge with en-US
    git-svn-id: http://svn.mozilla.org/addons/trunk@49188 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/locale/extract-po.sh
+++ site/app/locale/extract-po.sh
@@ -2,17 +2,25 @@
 SOURCE_DIRS="config models views controllers webroot"
 
 cd `dirname $0`/../
-touch messages.po
+touch ./locale/keys.pot
 
+echo -n "Extracting..."
 for sourcedir in $SOURCE_DIRS; do \
     find ./${sourcedir} -name "*thtml" -or -name "*.php" | xgettext \
         --language=PHP \
-        --keyword=___ \
+        --keyword=___:1 \
+        --keyword=___:1,2c \
         --keyword=n___:1,2 \
+        --keyword=n___:1,2,4c \
         --force-po \
         --omit-header \
         --join-existing \
         --sort-output \
+        --output=./locale/keys.pot \
         --copyright-holder="Mozilla Corporation" \
         --files-from=- # Pull from standard input (our find command) \
 done
+echo "done."
+echo -n "Merging to en-US"
+msgmerge -U ./locale/en_US/LC_MESSAGES/messages.po ./locale/keys.pot
+rm ./locale/keys.pot
