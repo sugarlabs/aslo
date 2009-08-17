ea7e8acddb8aadacc72d88c025ac3960fa2daf0a
ddash@mozilla.com <ddash@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-17 22:02:58 +0000

    [bug 504699] Cleaned up the search api and install
    git-svn-id: http://svn.mozilla.org/addons/trunk@49346 4eb1ac78-321c-0410-a911-ec516a8615a5

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -10,12 +10,12 @@ LATEST_SPHINX  = 'http://www.sphinxsearch.com/downloads/sphinx-0.9.9-rc2.tar.gz'
 SPHINX_VERSION = (LATEST_SPHINX.rpartition('/')[2]).rpartition('.tar.')[0]
 TARBALL        = os.path.join(TMP,LATEST_SPHINX.rpartition('/')[2])
 
-VAR_DIR     = '/var/local/'
+INSTALL_ROOT     = '/opt/local'
 
 def create_dirs():
     local("mkdir -p %s"% TMP)
-    local("mkdir -p %s" % os.path.join(SPHINX_DIR, 'data/sphinx'))
-    local("mkdir -p %s" % os.path.join(SPHINX_DIR, 'log/searchd"))
+    local("mkdir -p %s" % os.path.join(INSTALL_ROOT, 'data/sphinx'))
+    local("mkdir -p %s" % os.path.join(INSTALL_ROOT, 'log/searchd'))
 
 def install_sphinx():
     create_dirs()
@@ -23,4 +23,4 @@ def install_sphinx():
     local("wget -P %s %s" % (TMP, LATEST_SPHINX))
     local("cd %s; tar zxf %s" % (TMP, TARBALL))
     #make
-    local("cd %s;./configure --prefix=%s;make;make install"%(os.path.join(TMP, SPHINX_VERSION),ROOT))
+    local("cd %s;./configure --prefix=%s;make;make install"%(os.path.join(TMP, SPHINX_VERSION),INSTALL_ROOT))
