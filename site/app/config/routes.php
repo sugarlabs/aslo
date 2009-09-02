c82dfef0ca831ddabd33e5ae0ed770c66f4842ba
ddash@mozilla.com <ddash@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 23:23:14 +0000

    [bug 513700] Removing API1.5 as it's just a 1-off controller, that is duplicated in the main controller
    git-svn-id: http://svn.mozilla.org/addons/trunk@50749 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/config/routes.php
+++ site/app/config/routes.php
@@ -100,9 +100,6 @@
         $Route->connect("/{$prefix}/api/addon/*", array('controller' => 'api', 'action'=>'addon'));
         $Route->connect("/{$prefix}/api/list/*", array('controller' => 'api', 'action'=>'list_addons'));
 
-        // Search 1.5 API to use sphinx
-        $Route->connect("/{$prefix}/api/1.5/search/*", array('controller' => 'api15', 'action'=>'search'));
-
         // Add API versioning support
         $Route->connect("/{$prefix}/api/[\d\.]*/addon/*", array('controller' => 'api', 'action'=>'addon'));
         $Route->connect("/{$prefix}/api/[\d\.]*/list/*", array('controller' => 'api', 'action'=>'list_addons'));
