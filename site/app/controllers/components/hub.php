459d1831dc135e379e08956b88cd11010121c7ba
jscott@mozilla.com <jscott@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-03 00:31:40 +0000

    adding most of the how-to articles
    git-svn-id: http://svn.mozilla.org/addons/trunk@50762 4eb1ac78-321c-0410-a911-ec516a8615a5

--- site/app/controllers/components/hub.php
+++ site/app/controllers/components/hub.php
@@ -43,19 +43,195 @@ class HubComponent extends Object {
     function startup(&$controller) {
         $this->controller =& $controller;
 
-        $lorem = ___("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras posuere malesuada congue. Etiam dignissim lectus ut dui rhoncus elementum commodo orci sodales. Donec placerat tortor non nulla viverra euismod");
-
         $mdc = new HubSite('Mozilla Developer Center', 'http://developer.mozilla.org');
+        $wiki = new HubSite('Mozilla Wiki', 'http://wiki.mozilla.org');
+        $jetpack = new HubSite('Mozilla Labs - Jetpack', 'http://jetpack.mozillalabs.com');
 
         $this->categories = array(
-            new HubCategory('Getting Started', $lorem, 'getting-started', array(
+            new HubCategory('Getting Started', ___('Learn the basics of developing an extension on the Mozilla platform with this section full of beginner\'s guides.'), 'getting-started', array(
                 new SubCategory('The Basics', array(
                     new Howto(0, 'Firefox Add-ons Developer Guide',
                               'https://developer.mozilla.org/En/Firefox_addons_developer_guide',
                               $mdc,
-                              "In this detailed guide to extension development, you'll learn the basics of packaging extensions, building an interface with XUL, implementing advanced processes with XPCOM, and how to put it all together."),
+                              'In this detailed guide to extension development, you\'ll learn the basics of packaging extensions, building an interface with XUL, implementing advanced processes with XPCOM, and how to put it all together.'),
+                    new Howto(0, 'Setting Up an Extension Development Environment',
+                              'https://developer.mozilla.org/en/Setting_up_extension_development_environment',
+                              $mdc,
+                              'This article gives suggestions on how to set up your Mozilla application for extension development, including setting up a profile, changing preferences, and helpful development tools.'),
+                    new Howto(0, 'Building an Extension',
+                              'https://developer.mozilla.org/en/Building_an_Extension',
+                              $mdc,
+                              'This tutorial will take you through the steps required to build a very basic extension - one which adds a status bar panel to the Firefox browser containing the text "Hello, World!"'),
+                    new Howto(0, 'Extension Packaging',
+                              'https://developer.mozilla.org/en/Extension_Packaging',
+                              $mdc,
+                              'Learn more details on how extensions are packaged and installed.'),
+                    new Howto(0, 'Install Manifests',
+                              'https://developer.mozilla.org/en/Install_Manifests',
+                              $mdc,
+                              'This document explains install manifests (install.rdf) and all of properties available for use in your add-on.')
                 )),
+                new SubCategory('Other Tutorials', array(
+                    new Howto(0, 'How to Develop a Firefox Extension',
+                              'http://robertnyman.com/2009/01/24/how-to-develop-a-firefox-extension/',
+                              new HubSite('Robert\'s talk', 'http://www.robertnyman.com'),
+                              'In this walk-through, Robert Nyman explains how to develop a Firefox extension from scratch.'),
+                    new Howto(0, 'How to Build a Firefox Extension',
+                              'http://lifehacker.com/264490/how-to-build-a-firefox-extension',
+                              new HubSite('Lifehacker', 'http://www.lifehacker.com'),
+                              'Lifehacker gives tips and helpful hints on developing your first Firefox extension.'),
+                    new Howto(0, 'Firefox Extension Development Tutorial',
+                              'http://www.rietta.com/firefox/Tutorial/overview.html',
+                              new HubSite('Extend Firefox!', 'http://www.rietta.com/firefox/index.html'),
+                              'A multi-page tutorial covering a variety of extension development topics.')
+                )),
+                new SubCategory('Books', array(
+                    new Howto(0, 'Build Your Own Firefox Extension E-book',
+                              'http://www.sitepoint.com/books/byofirefoxpdf1/',
+                              new HubSite('SitePoint', 'http://www.sitepoint.com'),
+                              'With a little JavaScript know-how, author James Edwards will show you just how straightforward it is to build your own Firefox extensions.')
+                ))
             )),
+            new HubCategory('Extension Development', ___('Once you know the basics, these guides will help you with intermediate to advanced extension development topics.'), 'extension-development', array(
+                new SubCategory('Best Practices', array(
+                    new Howto(0, 'Respecting the JavaScript Global Namespace',
+                              'http://blogger.ziesemer.com/2007/10/respecting-javascript-global-namespace.html',
+                              new HubSite('Mark A. Ziesemer', 'http://blogger.ziesemer.com'),
+                              'In this blog post, Mark Ziesemer explains how to prevevent global namespace pollution by wrapping your extension\'s variables.'),
+                    new Howto(0, '@TODO Brian\'s Security Document',
+                              '#',
+                              $mdc,
+                              '@TODO '),
+                    new Howto(0, 'Localizing an Extension',
+                              'https://developer.mozilla.org/en/Localizing_an_extension',
+                              $mdc,
+                              'This article explains how to localize an extension, including XUL and JavaScript strings.')
+                )),
+                new SubCategory('Security', array(
+                    new Howto(0, 'Evaluating Code with Restricted Privileges',
+                              'https://developer.mozilla.org/en/Components.utils.evalInSandbox',
+                              $mdc,
+                              'This article describes the use of Components.utils.evalInSandbox, which is a way to evaluate code (such as remote code) without chrome privileges.'),
+                    new Howto(0, 'Creating Sandboxed HTTP Connections',
+                              'https://developer.mozilla.org/En/Creating_Sandboxed_HTTP_Connections',
+                              $mdc,
+                              'This article explains how to created sandboxed HTTP connections which don\'t affect the user\'s cookies.'),
+                    new Howto(0, 'Displaying Web Content in an Extension',
+                              'https://developer.mozilla.org/En/Displaying_web_content_in_an_extension_without_security_issues',
+                              $mdc,
+                              'Learn how to display web content in an extension without security issues.'),
+                    new Howto(0, 'Five Wrong Reasons to Use eval() in an Extension',
+                              'http://adblockplus.org/blog/five-wrong-reasons-to-use-eval-in-an-extension',
+                              new HubSite('Adblock Plus Blog', 'http://www.adblockplus.org'),
+                              'In this blog post, Wladimir Palant gives five wrong ways to evaluate code in an extension.')
+                )),
+                new SubCategory('Localization', array(
+                    new Howto(0, 'Localizing Extension Descriptions',
+                              'https://developer.mozilla.org/en/Localizing_extension_descriptions',
+                              $mdc,
+                              'Learn how to localize the names and descriptions in extension install manifests.'),
+                    new Howto(0, 'Localization and Plurals',
+                              'https://developer.mozilla.org/en/Localization_and_Plurals',
+                              $mdc,
+                              'This article explains how to properly localize strings with plurals.')
+                )),
+                new SubCategory('Advanced Topics', array(
+                    new Howto(0, 'Creating Custom Firefox Extensions with the Mozilla Build System',
+                              'https://developer.mozilla.org/en/Creating_Custom_Firefox_Extensions_with_the_Mozilla_Build_System',
+                              $mdc,
+                              'This article describes how to set up the development environment for a large, complex Firefox extension with a need for high-performance, use of third-party libraries in C/C++, or interfaces not exposed via XPCOM.'),
+                    new Howto(0, 'Multiple Item Packaging',
+                              'https://developer.mozilla.org/en/Multiple_Item_Packaging',
+                              $mdc,
+                              'This article explains how to create an extension package with multiple items (extensions).')
+                ))
+            )),
+            new HubCategory('Thunderbird &amp; Mobile Add-ons', ___('Add-ons aren\'t just for Firefox. Learn how to extend other Mozilla applications, such as the Thunderbird mail client and Firefox for mobile devices.'), 'thunderbird-mobile', array(
+                new SubCategory('Developing Add-ons for Thunderbird', array(
+                    new Howto(0, 'Building a Thunderbird Extension',
+                              'https://developer.mozilla.org/en/Building_a_Thunderbird_extension',
+                              $mdc,
+                              'This tutorial will take you through the steps required to build a very basic extension - one which adds a status bar panel to the Thunderbird Mail Client containing the text "Hello, World!"'),
+                    new Howto(0, 'An Overview of Thunderbird Components',
+                              'https://developer.mozilla.org/En/Extensions/Thunderbird/An_overview_of_the_Thunderbird_interface',
+                              $mdc,
+                              'This article describes the Thunderbird user interface, discusses example modifications, and introduces some of the APIs most commonly used by extension developers.'),
+                    new Howto(0, 'Thunderbird How-tos',
+                              'https://developer.mozilla.org/en/Extensions/Thunderbird/HowTos',
+                              $mdc,
+                              'This section contains many code samples and how-to articles on common Thunderbird extension tasks.'),
+                    new Howto(0, 'Thunderbird Extensions Documentation Index',
+                              'https://developer.mozilla.org/en/Extensions/Thunderbird',
+                              $mdc,
+                              'If you can\'t find what you\'re looking for in the above articles, try the Mozilla Developer Center\'s Thunderbird extensions landing page.'),
+                )),
+                new SubCategory('Developing Add-ons for Mobile', array(
+                    new Howto(0, 'Mobile Architecture',
+                              'https://wiki.mozilla.org/Mobile/Fennec/Architecture',
+                              $wiki,
+                              'This document describes the architecture of Firefox on mobile and includes performance tips for mobile code.'),
+                    new Howto(0, 'Mobile Extensions',
+                              'https://wiki.mozilla.org/Mobile/Fennec/Extensions',
+                              $wiki,
+                              'This document explains the specifics on creating an extension for mobile.'),
+                    new Howto(0, 'Best Practices for Mobile Extensions',
+                              'https://wiki.mozilla.org/Mobile/Fennec/Extensions/BestPractices',
+                              $wiki,
+                              'Mobile extensions have small screen space and limited resources to work with. This document explains best practices for designing and developing for a mobile environment.'),
+                    new Howto(0, 'Mobile Code Snippets',
+                              'https://wiki.mozilla.org/Mobile/Fennec/CodeSnippets',
+                              $wiki,
+                              'Code snippets specific to mobile.')
+                ))
+            )),
+            new HubCategory('Theme Development', ___('Style Mozilla applications the way you want with pixel-perfect themes.'), 'theme-development', array(
+                /*new SubCategory('Best Practices', array(
+                    new Howto(0, 'Respecting the JavaScript Global Namespace',
+                              'http://blogger.ziesemer.com/2007/10/respecting-javascript-global-namespace.html',
+                              new HubSite('Mark A. Ziesemer', 'http://blogger.ziesemer.com'),
+                              'In this blog post, Mark Ziesemer explains how to prevevent global namespace pollution by wrapping your extension\'s variables.')
+                )),
+                new SubCategory('Advanced Topics', array(
+                    new Howto(0, 'Creating Custom Firefox Extensions with the Mozilla Build System',
+                              'https://developer.mozilla.org/en/Creating_Custom_Firefox_Extensions_with_the_Mozilla_Build_System',
+                              $mdc,
+                              'This article describes how to set up the development environment for a large, complex Firefox extension with a need for high-performance, use of third-party libraries in C/C++, or interfaces not exposed via XPCOM.'),
+                    new Howto(0, 'Multiple Item Packaging',
+                              'https://developer.mozilla.org/en/Multiple_Item_Packaging',
+                              $mdc,
+                              'This article explains how to create an extension package with multiple items (extensions).')
+                ))*/
+            )),
+            new HubCategory('Other Types of Add-ons', ___('Find information on Search Plug-ins, Jetpack, Personas, and other types of add-ons here.'), 'other-addons', array(
+                new SubCategory('Jetpack', array(
+                    new Howto(0, 'Jetpack Tutorial',
+                              'https://jetpack.mozillalabs.com/tutorial.html',
+                              $jetpack,
+                              'Learn how easy it is extend Firefox using Jetpack with this tutorial.'),
+                    new Howto(0, 'Jetpack API Documentation',
+                              'https://jetpack.mozillalabs.com/api.html',
+                              $jetpack,
+                              'This Jetpack API documentation will come in handy when developing your Jetpacks.')
+                )),
+                new SubCategory('Personas', array(
+                    new Howto(0, 'How to Create Personas',
+                              'http://getpersonas.com/demo_create',
+                              new HubSite('Personas for Firefox', 'http://www.getpersonas.com'),
+                              'Learn how to create your very own Persona with this official walk-through.')
+                )),
+                new SubCategory('Search Plug-ins', array(
+                    new Howto(0, 'Creating OpenSearch Plug-ins for Firefox',
+                              'https://developer.mozilla.org/en/Creating_OpenSearch_plugins_for_Firefox',
+                              $mdc,
+                              'This article explains the structure and requirements of search plug-ins in Firefox.')
+                )),
+                new SubCategory('Plug-ins', array(
+                    new Howto(0, 'Plug-in Documentation Index',
+                              'https://developer.mozilla.org/en/Plugins',
+                              $mdc,
+                              'Plug-ins to Mozilla-based applications are binary components that can display content that the application itself can\'t display natively. Explore the Mozilla Developer Center\'s plug-in documentation index to learn more.')
+                )),
+            ))
         );
 
         $this->policies = array(
@@ -66,7 +242,8 @@ class HubComponent extends Object {
             new HubCategory('Developer Agreement', ___('Terms of Service for submitting your work to our site. Developers are required to accept this agreement before submission.'), 'agreement'),
             new HubCategory('Contacting Us', ___('How to get in touch with the AMO team regarding these policies or your add-on.'), 'contact')
         );
-
+        
+        $lorem = 'foo';
         $this->casestudies = array(
             new HubCaseStudy('Cooliris lorem ipsum', $lorem, 'cooliris', 5579,
                 '/img/amo2009/logo-firefox.gif', ___('Cooliris action text')),
