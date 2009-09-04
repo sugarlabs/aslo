41b0fa947d3a25ffc1d62fa4150fa2c7dbc285b4
jbalogh@mozilla.com <jbalogh@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-04 23:11:34 +0000

    bug 509775, only print messages if debug is set in the db
    git-svn-id: http://svn.mozilla.org/addons/trunk@50940 4eb1ac78-321c-0410-a911-ec516a8615a5

--- bin/build-recommendations.py
+++ bin/build-recommendations.py
@@ -14,6 +14,7 @@ CONNECTION = 'php connection.php %s'
 SELECT = 'SELECT collection_id, addon_id FROM addons_collections ORDER BY collection_id'
 INSERT = 'INSERT INTO addon_recommendations VALUES '
 DELETE = 'TRUNCATE addon_recommendations'
+SELECT_DEBUG = 'SELECT value FROM config WHERE config.key="cron_debug_enabled"'
 
 
 def call(cmd):
@@ -44,11 +45,21 @@ def main():
     read, write = [db_config(json.loads(call(CONNECTION % s)))
                    for s in ('read', 'write')]
 
+    with mysql.connect(**read) as cursor:
+        if cursor.execute(SELECT_DEBUG) and cursor.fetchone()[0] == '1':
+            def debug(msg):
+                print msg
+        else:
+            debug = lambda msg: None
+
     d = map_data(mysql.connect(**read).cursor())
 
     inserts = []
     recs = recommend.top(recommend.rank_all(d))
     for index, (addon, scores) in enumerate(recs):
+        if index % 100 == 0:
+            debug('%s: %s (%s)' % (index, addon, time.time() - start))
+
         inserts.extend((addon, other, score) for other, score in scores)
 
     with mysql.connect(**write) as cursor:
@@ -57,6 +68,7 @@ def main():
         cursor.execute(INSERT + ','.join('(%s,%s,%s)' % x for x in inserts))
         cursor.execute('COMMIT')
 
+    debug('Total: %s' % (time.time() - start))
 
 if __name__ == '__main__':
     main()
